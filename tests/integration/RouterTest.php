e47095fb49da077ca57da168b59a19dbdeaa8625
Alex Ash <streamprop@gmail.com>
2016-03-02 16:14:49 +0300

    Repair dev autoloading, move assets to assets directories refs #4

--- tests/integration/RouterTest.php
+++ tests/integration/RouterTest.php
@@ -11,17 +11,18 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 {
     public function testRoute_WithValidRequest_MapToAppropriateController()
     {
-        $router = $this->getRouter('Integration');
+        $rootNamespace = 'Integration\\Assets\\Controllers';
+        $router = $this->getRouter($rootNamespace);
         $uri = new Uri('http://example.com/example/action/?a=foo&b=bar');
         $httpMethod = 'GET';
 
         $controllerInvoker = $router->route($uri, $httpMethod);
 
         $expected = [
-            'class'  => 'Integration\\ExampleController',
+            'class'  => $rootNamespace.'\\ExampleController',
             'method' => 'getAction',
             'params' => ['a' => 'foo', 'b' => 'bar'],
-            'result' => 'Integration\\ExampleController::getAction(foo, bar)',
+            'result' => $rootNamespace.'\\ExampleController::getAction(foo, bar)',
         ];
         $this->assertEquals($expected['class'],  $controllerInvoker->getClass());
         $this->assertEquals($expected['method'], $controllerInvoker->getMethod());
@@ -31,17 +32,18 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 
     public function testRoute_WithThreeUriPathSegments_MapToAppropriateController()
     {
-        $router = $this->getRouter('');
-        $uri = new Uri('http://example.com/integration/example/action/?a=foo&b=bar');
+        $rootNamespace = 'Integration\\Assets\\Controllers';
+        $router = $this->getRouter($rootNamespace);
+        $uri = new Uri('http://example.com/special/example/action/?a=foo&b=bar');
         $httpMethod = 'GET';
 
         $controllerInvoker = $router->route($uri, $httpMethod);
 
         $expected = [
-            'class'  => 'Integration\\ExampleController',
+            'class'  => $rootNamespace.'\\Special\\ExampleController',
             'method' => 'getAction',
             'params' => ['a' => 'foo', 'b' => 'bar'],
-            'result' => 'Integration\\ExampleController::getAction(foo, bar)',
+            'result' => $rootNamespace.'\\Special\\ExampleController::getAction(foo, bar)',
         ];
         $this->assertEquals($expected['class'],  $controllerInvoker->getClass());
         $this->assertEquals($expected['method'], $controllerInvoker->getMethod());
@@ -52,16 +54,5 @@ class RouterTest extends \PHPUnit_Framework_TestCase
     protected function getRouter($rootNamespace)
     {
         return \Traveler\Bootstrap\bootstrap($rootNamespace)->get('Traveler\\Router');
-    }
-}
-
-/**
- * @author Alex Ash <streamprop@gmail.com>
- */
-class ExampleController
-{
-    public function getAction($alpha, $betta)
-    {
-        return "Integration\\ExampleController::getAction($alpha, $betta)";
     }
 }
--- tests/unit/Invokers/ControllerInvokerTest.php
+++ tests/unit/Invokers/ControllerInvokerTest.php
@@ -12,20 +12,20 @@ class ControllerInvokerTest extends \PHPUnit_Framework_TestCase
     public function testCall_WithExistingController_CallController()
     {
         $invoker = new ControllerInvoker();
-        $invoker->setClass('Unit\\Invokers\\ExampleController');
+        $invoker->setClass('Unit\\Assets\\Controllers\\ExampleController');
         $invoker->setMethod('getAction');
         $invoker->setParams(['a' => 'foo', 'b' => 'bar']);
 
         $result = $invoker();
 
-        $expected = "Unit\\Invokers\\ExampleController::getAction(foo, bar)";
+        $expected = "Unit\\Assets\\Controllers\\ExampleController::getAction(foo, bar)";
         $this->assertEquals($expected, $result);
     }
 
     public function testCall_WithNonExistingController_ThrowException404()
     {
         $invoker = new ControllerInvoker();
-        $invoker->setClass('Unit\\Invokers\\BadexampleController');
+        $invoker->setClass('Unit\\Assets\\Controllers\\BadexampleController');
         $invoker->setMethod('getAction');
         $invoker->setParams(['a' => 'foo', 'b' => 'bar']);
 
@@ -36,22 +36,11 @@ class ControllerInvokerTest extends \PHPUnit_Framework_TestCase
     public function testCall_WithNonExistingAction_ThrowException404()
     {
         $invoker = new ControllerInvoker();
-        $invoker->setClass('Unit\\Invokers\\ExampleController');
+        $invoker->setClass('Unit\\Assets\\Controllers\\ExampleController');
         $invoker->setMethod('getNothing');
         $invoker->setParams(['a' => 'foo', 'b' => 'bar']);
 
         $this->setExpectedException('\\Traveler\\Invokers\\Exceptions\\Exception404');
         $invoker();
-    }
-}
-
-/**
- * @author Alex Ash <streamprop@gmail.com>
- */
-class ExampleController
-{
-    public function getAction($alpha, $betta)
-    {
-        return "Unit\\Invokers\\ExampleController::getAction($alpha, $betta)";
     }
 }
